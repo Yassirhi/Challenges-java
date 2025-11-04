fd166a7e441a5877183e0ad9c3623aa6a214a702
unknown <ysr.hider@gmail.com>
2025-11-04 17:20:32 +0100

    first commit

--- 2PlusGran/src/Main.java
+++ 2PlusGran/src/Main.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String d;
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> nombres = new ArrayList<>();
         do{
             System.out.println("Entrer la valeur :");
-            Scanner scanner = new Scanner(System.in);
-
             int  v = scanner.nextInt();
             nombres.add(v);
             System.out.println("Voulez-vous ajouter un autre valeur :(o/n)");
-            d = scanner.nextLine();
+            d = scanner.next();
 
-        }while(d == "o");
-        int[] maxi = new int[2];
-        int max = 0;
+        }while(d.equals("o"));
+        //int[] maxi = new int[2];
+        int max = 0,k=0;
         for (int i = 0; i < nombres.size(); i++) {
-                if(nombres.get(i) >max){
-                    max = nombres.get(i);
-                    maxi[0] = max;
-
-                }
+            if (nombres.get(i) > max) {
+                k= i;
+            }
+        }
+        for (int j = 0; j < nombres.size(); j++) {
+            if(j!=k){
+                max=nombres.get(j);
+            }
+            }
+        System.out.println(max);
         }
     }
-}
